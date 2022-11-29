8b12f87571083220fc47dd7a4546c4b135e0e434
waylen_roche <waylenroche@gmail.com>
2022-11-29 17:13:45 -0500

    login show on main

--- ghi/src/components/TriviaGame.jsx
+++ ghi/src/components/TriviaGame.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { format } from "date-fns"
 import Button from "@mui/material/Button";
 import parse from "html-react-parser"; // coverts html into string
@@ -17,10 +17,14 @@ import "../trivia.css";
 const API_URL = "https://opentdb.com/api.php?amount=10";
 
 const TriviaGame = () => {
+
+  // state for user game api
   const { data: tokenData } = useGetTokenQuery();
   const [createFinalScore, result] = useAddScoreMutation()
   const [category, setCategory] = useState('Random')
   const [queryDifficulty, setQueryDifficulty] = useState('Mixed')
+
+  // game play state
   let [data, setData] = useState([]); // -> The whole json response from API Trivia
   let [count, setCount] = useState(0);
   let [score, setScore] = useState(0);
@@ -31,6 +35,57 @@ const TriviaGame = () => {
   let [possibleAnswers, setPossibleAnswers] = useState([]) // List of all answers ( correct + incorrect ones) for a specific question
   let [gameEnded, setGameEnded] = useState(false);
   let [isAnswerSelected, setIsAnswerSelected] = useState(false);
+
+  //timer state
+  const Ref = useRef(null);
+  const [timer, setTimer] = useState('00:15');
+
+  //timer functionality
+  const getTimeRemaining = (e) => {
+    const total = Date.parse(e) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    return {
+      total, minutes, seconds
+    };
+  }
+
+
+  const startTimer = (e) => {
+    let { total, minutes, seconds }
+      = getTimeRemaining(e);
+    if (total === 0) {
+      incrementCount();
+      setTimeout(onClickStart(), 3000)
+      getQuestion();
+    }
+    if (total >= 0) {
+      setTimer(
+        (minutes > 9 ? minutes : '0' + minutes) + ':'
+        + (seconds > 9 ? seconds : '0' + seconds)
+      )
+    }
+  }
+
+
+  const clearTimer = (e) => {
+    setTimer('00:15');
+    if (Ref.current) clearInterval(Ref.current);
+    const id = setInterval(() => {
+      startTimer(e);
+    }, 500)
+    Ref.current = id;
+  }
+
+  const getDeadTime = () => {
+    let deadline = new Date();
+    deadline.setSeconds(deadline.getSeconds() + 15);
+    return deadline;
+  }
+
+  const onClickStart = () => {
+    clearTimer(getDeadTime());
+  }
 
   const currentDate = new Date()
   const formattedDate = format(currentDate, "yyyy-MM-dd")
@@ -85,14 +140,10 @@ const TriviaGame = () => {
       setDifficulty(difficulty);
       setCorrectAnswer(correct_answer);
       setPossibleAnswers(shuffle([...incorrect_answers, correct_answer]));
+      onClickStart();
     } catch (e) {
       setGameEnded(true);
       createFinalScore({ formattedDate, category, queryDifficulty, score })
-      // if (result.isSuccess) {
-      //   setGameEnded(true);
-      // } else if (result.isError) {
-      //   console.log(result.error)
-      // }
     }
   };
 
@@ -116,6 +167,7 @@ const TriviaGame = () => {
   };
 
   const startQuiz = () => {
+    onClickStart();
     setQuizStarted(true);
     getQuestion();
   };
@@ -140,8 +192,6 @@ const TriviaGame = () => {
       selectedAnswerButtonEl.classList.remove("correct_btn");
       selectedAnswerButtonEl.classList.remove("wrong_btn");
     }, 1950);
-    // if question timer goes to 0: incrementCount()
-    //or if answer is chosen before timer goes to 0:
     incrementCount();
   };
 
@@ -165,66 +215,71 @@ const TriviaGame = () => {
   } else {
     return (
       //hid the game div until category is selected, and start/play is pressed
-      <div className="App">
-        {!gameEnded ? (
-          <div className="ended game">
-            {quizStarted ? (
-              <div className="container">
-                <Button className="font_large" variant="contained">
-                  {" "}
-                  {parse(question)}{" "}
-                </Button>
-                <div className="div_possible_answers">
-                  {possibleAnswers.map((ans, idx) => {
-                    // for answer in response_answer make button
-                    return (
-                      <div className="possbile_answer_div" key={btoa(ans) + idx}>
-                        <Button
-                          className="possbile_answer font_large"
-                          onClick={() => onSelectAnswer(idx, ans)}
-                          id={idx}
-                          variant="contained"
-                          keyprop={idx}
-                        >
-                          {" "}
-                          {parse(ans)}{" "}
-                        </Button>
-                      </div>
-                    );
-                  })}
+      <>
+        <div className="App">
+          <h2>{timer}</h2>
+        </div>
+        <div className="App">
+          {!gameEnded ? (
+            <div className="ended game">
+              {quizStarted ? (
+                <div className="container">
+                  <Button className="font_large" variant="contained">
+                    {" "}
+                    {parse(question)}{" "}
+                  </Button>
+                  <div className="div_possible_answers">
+                    {possibleAnswers.map((ans, idx) => {
+                      // for answer in response_answer make button
+                      return (
+                        <div className="possbile_answer_div" key={btoa(ans) + idx}>
+                          <Button
+                            className="possbile_answer font_large"
+                            onClick={() => onSelectAnswer(idx, ans)}
+                            id={idx}
+                            variant="contained"
+                            keyprop={idx}
+                          >
+                            {" "}
+                            {parse(ans)}{" "}
+                          </Button>
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
-              </div>
-            ) : (
+              ) : (
+                <Button
+                  className="font_large"
+                  variant="contained"
+                  onClick={startQuiz}
+                >
+                  {" "}
+                  Start Quiz{" "}
+                </Button>
+              )}
               <Button
                 className="font_large"
                 variant="contained"
-                onClick={startQuiz}
+                onClick={showState}
               >
                 {" "}
-                Start Quiz{" "}
+                PRINT STATE{" "}
               </Button>
-            )}
-            <Button
-              className="font_large"
-              variant="contained"
-              onClick={showState}
-            >
-              {" "}
-              PRINT STATE{" "}
-            </Button>
-            <div className="score_container">
-              <Button className="font_large score_btn" variant="contained">
-                {" "}
-                {score}{" "}
-              </Button>
+              <div className="score_container">
+                <Button className="font_large score_btn" variant="contained">
+                  {" "}
+                  {score}{" "}
+                </Button>
+              </div>
             </div>
-          </div>
-        ) : (
-          <div>
-            <h1> ENDED GAME! Your final score is: {score} !</h1>
-          </div>
-        )}
-      </div>
+          ) : (
+            <div>
+              <h1> ENDED GAME! Your final score is: {score} !</h1>
+            </div>
+          )}
+        </div>
+      </>
     );
   }
 }
