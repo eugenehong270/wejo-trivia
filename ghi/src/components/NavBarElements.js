396937248b7fe7b9ceaf95953f1ffa7b4dbcf9f8
Eugene Hong <ehong270@gmail.com>
2022-12-06 16:23:26 -0500

    minor improvements

--- ghi/src/components/NavBarElements.js
+++ ghi/src/components/NavBarElements.js
@@ -30,61 +30,18 @@ export const NavLink = styled(Link)`
   }
 `;
 
-export const Bars = styled(FaBars)`
-  display: none;
-  color: #fff;
-  @media screen and (max-width: 768px) {
-    display: block;
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(-100%, 75%);
-    font-size: 1.8rem;
-    cursor: pointer;
-  }
-`;
+
 
 export const NavMenu = styled.div`
   display: flex;
   align-items: right;
   margin-right: -190px;
   /* Second Nav */
-  /* margin-right: 24px; */
+  /* margin-right: 800px; */
   /* Third Nav */
   /* width: 100vw;
   white-space: nowrap; */
   @media screen and (max-width: 768px) {
     display: none;
-  }
-`;
-
-export const NavBtn = styled.nav`
-  display: flex;
-  align-items: center;
-  margin-right: 24px;
-  /* Third Nav */
-  /* justify-content: flex-end;
-  width: 100vw; */
-  @media screen and (max-width: 768px) {
-    display: none;
-  }
-`;
-
-export const NavBtnLink = styled(Link)`
-  border-radius: 4px;
-  background: #256ce1;
-  padding: 10px 22px;
-  color: #fff;
-  outline: none;
-  border: none;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  text-decoration: none;
-  /* Second Nav */
-  margin-left: 24px;
-  &:hover {
-    transition: all 0.2s ease-in-out;
-    background: #fff;
-    color: #010606;
   }
 `;
